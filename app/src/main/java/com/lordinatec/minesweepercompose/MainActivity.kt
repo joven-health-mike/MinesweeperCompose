d9ceb78f09e553e047212bc80f44f89692804ab9
Mike Burke <mike@jovenhealth.com>
2024-08-11 18:33:39 -0600

    Replace icon in MainActivity

--- app/src/main/java/com/lordinatec/minesweepercompose/MainActivity.kt
+++ app/src/main/java/com/lordinatec/minesweepercompose/MainActivity.kt
@@ -12,17 +12,16 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.WorkspacePremium
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.lordinatec.minesweepercompose.ui.theme.MinesweeperComposeTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,15 +36,16 @@ class MainActivity : ComponentActivity() {
                                 .padding(innerPadding)
                                 .fillMaxWidth(), contentAlignment = Alignment.Center
                         ) {
-
-                            Icon(
-                                imageVector = Icons.Filled.WorkspacePremium,
-                                contentDescription = "",
-                                tint = colorResource(android.R.color.darker_gray),
-                                modifier = Modifier
-                                    .width(200.dp)
-                                    .height(200.dp)
-                            )
+                            Column {
+                                Spacer(modifier = Modifier.height(200.dp))
+                                Icon(
+                                    painterResource(R.drawable.android_icon_foreground_minesweeper),
+                                    "app icon",
+                                    modifier = Modifier
+                                        .width(200.dp)
+                                        .height(200.dp)
+                                )
+                            }
                         }
 
                         Spacer(modifier = Modifier.height(20.dp))

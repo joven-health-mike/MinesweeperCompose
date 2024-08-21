abfb9dea5975d2bccab945c9cb6d2b4d161eae2a
Mike Burke <mike@jovenhealth.com>
2024-08-20 19:20:35 -0600

    Handle MainActivity orientation change

--- app/src/main/java/com/lordinatec/minesweepercompose/MainActivity.kt
+++ app/src/main/java/com/lordinatec/minesweepercompose/MainActivity.kt
@@ -1,14 +1,15 @@
 package com.lordinatec.minesweepercompose
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
@@ -16,8 +17,10 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.lordinatec.minesweepercompose.ui.theme.MinesweeperComposeTheme
@@ -30,47 +33,70 @@ class MainActivity : ComponentActivity() {
         setContent {
             MinesweeperComposeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Column {
-                        Box(
-                            modifier = Modifier
-                                .padding(innerPadding)
-                                .fillMaxWidth(), contentAlignment = Alignment.Center
-                        ) {
-                            Column {
-                                Spacer(modifier = Modifier.height(200.dp))
-                                Icon(
-                                    painterResource(R.drawable.android_icon_foreground_minesweeper),
-                                    "app icon",
-                                    modifier = Modifier
-                                        .width(200.dp)
-                                        .height(200.dp)
-                                )
-                            }
-                        }
-
-                        Spacer(modifier = Modifier.height(20.dp))
-
-                        Box(
-                            modifier = Modifier
-                                .padding(innerPadding)
-                                .fillMaxWidth(), contentAlignment = Alignment.Center
-                        ) {
-                            Column {
-                                Button(
-                                    modifier = Modifier.width(250.dp),
-                                    onClick = { GameActivity.launch(applicationContext) }) {
-                                    Text("New Game")
-                                }
-                                Button(
-                                    modifier = Modifier.width(250.dp),
-                                    onClick = { finish() }) {
-                                    Text("Exit")
-                                }
-                            }
-                        }
+                    val isPortraitMode =
+                        LocalConfiguration.current.orientation == Configuration.ORIENTATION_PORTRAIT
+                    if (isPortraitMode) {
+                        PortraitScreen(modifier = Modifier.padding(innerPadding))
+                    } else {
+                        LandscapeScreen(modifier = Modifier.padding(innerPadding))
                     }
                 }
             }
         }
+    }
+
+    @Composable
+    fun PortraitScreen(modifier: Modifier) {
+        Box(modifier = Modifier.fillMaxSize()) {
+            Column(
+                modifier = modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceEvenly
+            ) {
+                AppIcon()
+                MenuButtons()
+            }
+        }
+    }
+
+    @Composable
+    fun LandscapeScreen(modifier: Modifier) {
+        Box(modifier = Modifier.fillMaxSize()) {
+            Row(
+                modifier = modifier.fillMaxSize(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                AppIcon()
+                MenuButtons()
+            }
+        }
+    }
+
+    @Composable
+    private fun MenuButtons() {
+        Column {
+            Button(
+                modifier = Modifier.width(250.dp),
+                onClick = { GameActivity.launch(applicationContext) }) {
+                Text("New Game")
+            }
+            Button(
+                modifier = Modifier.width(250.dp),
+                onClick = { finish() }) {
+                Text("Exit")
+            }
+        }
+    }
+
+    @Composable
+    private fun AppIcon() {
+        Icon(
+            painterResource(R.drawable.android_icon_foreground_minesweeper),
+            "app icon",
+            modifier = Modifier
+                .width(200.dp)
+                .height(200.dp)
+        )
     }
 }

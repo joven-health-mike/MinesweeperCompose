7d589dd516d14a12219f59b6bddce537cc8ffef3
Mike Burke <mike@jovenhealth.com>
2024-09-05 16:46:43 -0600

    Update GameActivityTest.kt

--- app/src/androidTest/java/com/lordinatec/minesweepercompose/gameplay/GameActivityTest.kt
+++ app/src/androidTest/java/com/lordinatec/minesweepercompose/gameplay/GameActivityTest.kt
@@ -2,29 +2,25 @@
  * Copyright Lordinatec LLC 2024
  */
 
-package com.lordinatec.minesweepercompose.stats
+package com.lordinatec.minesweepercompose.gameplay
 
 import android.content.Context
 import androidx.test.core.app.ActivityScenario
 import androidx.test.platform.app.InstrumentationRegistry
 import com.lordinatec.minesweepercompose.config.Config
-import com.lordinatec.minesweepercompose.gameplay.GameActivity
 import junit.framework.TestCase.assertEquals
 import org.junit.Before
 import org.junit.Test
 
-
 class GameActivityTest {
 
     private lateinit var context: Context
-    private lateinit var statsUpdater: StatsUpdater
 
     @Before
     fun setUp() {
         context = InstrumentationRegistry.getInstrumentation().targetContext
         context.getSharedPreferences("MinesweeperComposePrefs", Context.MODE_PRIVATE).edit().clear()
             .apply()
-        statsUpdater = StatsUpdater(context)
         Config.feature_adjust_field_to_screen_size = true
         Config.feature_end_game_on_last_flag = true
     }

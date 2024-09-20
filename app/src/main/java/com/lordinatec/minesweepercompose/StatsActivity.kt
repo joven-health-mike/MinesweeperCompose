e9969d03e15cc5d150cd5f7002585803e4f97ece
Mike Burke <103524022+joven-health-mike@users.noreply.github.com>
2024-09-19 21:39:19 -0600

    Release/2.0 (#79)
    * Dev Merge (#50)
    
    * Shake the field on game lost
    
    * Comments + slight refactor
    
    * First run at a drop-down animation on all tiles
    
    * Fixed issues with drop-down animation. Pretty stable now.
    
    * Make all tiles visible when the game is started
    
    * Add stats page + ability to save best time
    
    * Add stats for wins and losses
    
    * Fix issue with field not dropping down when tile clicked before animations are complete
    
    * Add mockito to dependencies
    
    * Create GameListenerBridgeTest.kt
    
    * Update GameListenerBridgeTest.kt
    
    * Add MockK dependency
    
    * Add GameViewModel tests
    
    * Change from junit to kotlin tests
    
    * Update GameViewModelTest.kt
    
    * Improve some logistic logic with the viewmodel
    
    * (broken) Create a new starting point
    
    * Making some progress...
    
    * Finish testing GameViewModel
    
    * Fix problems + create unit test for GameEventPublisher
    
    * Revert last changes to GameEventPublisher
    
    * Fix & finish GameEventPublisherTests
    
    * Add tests for GameController
    
    * Fix some weird issues
    
    * Fixed remaining issues
    
    * Fix failing tests
    
    * Update release name and code
    
    * Release 2.0 merge (#77)
    
    * Implement Dagger Hilt (#69)
    
    * Implement Dagger Hilt on GameViewModel
    
    * Add EOF new lines
    
    * Remove circular dependency on timer functionality
    
    * Comment Timer.kt
    
    * Update libs.versions.toml
    
    * Dagger hilt game module (#70)
    
    * (Broken) Implement dagger hilt on Game Module
    
    * Fix bug when game activity is restarted + documentation
    
    * Comments
    
    * Update unit tests and cleanup
    
    * Log game events to logcat + add new "ResetField" event
    
    * Remove dependency on mines-java (#71)
    
    * Remove references to jar file + remove libs folder from build
    
    * Fix functionality after removing jar file
    
    * Comments and cleanup
    
    * Fix unit tests
    
    * Remove XYIndexTranslator dependencies
    
    * Fix unit tests
    
    * Fix issue with field configuration
    
    * Cleanup
    
    * Simplify adjacent apis and updating field configuration
    
    * Rename CoordinateFactoryImpl to CachedCoordinateFactory
    
    * Fix issues with game events coming out of order and mines remaining not counting correctly
    
    * Run events on the main dispatcher
    
    * Inject dispatcher to ViewModel
    
    * Simplify event publisher
    
    * Decouple state updates from the viewmodel + update README
    
    * Fix linter and problems warnings
    
    * Extract timer functionality into an event consumer, controller tests, refactoring
    
    * Update TimerLifecycleObserverTest.kt
    
    * Refactor eventPublisher, such that it can be read only by classes that shouldn't be sending events
    
    * Get rid of XY coordinates and replace with just indexes (#75)
    
    * Remove all references to XY coordinates and change everything to indexes
    
    * Fix unit tests
    
    * Fix bug with "bottom" calculation on adjacent
    
    * Create game on tile flagged
    
    * Fix issue with game not restarting on new game clicked
    
    * Refactoring for readability
    
    * Remove gameplay.apis package and create AdjacentTest
    
    * Unit testing
    
    * Address project problems
    
    * Update README.md

---
+++ app/src/main/java/com/lordinatec/minesweepercompose/StatsActivity.kt
@@ -0,0 +1,77 @@
+/*
+ * Copyright Lordinatec LLC 2024
+ */
+
+package com.lordinatec.minesweepercompose
+
+import android.content.Context
+import android.content.Intent
+import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.core.content.ContextCompat.startActivity
+
+class StatsActivity : ComponentActivity() {
+    private val activityContext = this
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Top
+            ) {
+                val wins = Stats.getWins(activityContext)
+                val losses = Stats.getLosses(activityContext)
+                Spacer(modifier = Modifier.fillMaxSize(0.1f))
+                Text("Stats")
+                Text(
+                    "Best Time: ${
+                        formatFloat(
+                            Stats.getBestTime(activityContext) / 1000f,
+                            3
+                        )
+                    } seconds"
+                )
+                Text("Wins: $wins")
+                Text("Losses: $losses")
+                Text(
+                    "Win Rate: ${
+                        if (wins + losses == 0) "0.0" else formatFloat(
+                            wins.toFloat() * 100 / (wins + losses),
+                            1
+                        )
+                    }%"
+                )
+            }
+        }
+    }
+
+    private fun formatFloat(value: Float, padding: Int): String {
+        val intValue = value.toInt()
+        var decValue = (value - intValue).toString().padEnd(padding, '0')
+        val maxLength = 2 + padding
+        decValue =
+            decValue.substring(2, if (decValue.length > maxLength) maxLength else decValue.length)
+        return "$intValue.$decValue"
+    }
+
+    companion object {
+        fun launch(context: Context) {
+            val intent = Intent(context, StatsActivity::class.java)
+            intent.setFlags(FLAG_ACTIVITY_NEW_TASK)
+            startActivity(context, intent, null)
+        }
+    }
+}
